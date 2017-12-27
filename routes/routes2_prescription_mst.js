eba09921db8a52583a1eba96ce4e3b80977e6c4e
malavshah9 <mdshah9574@gmail.com>
2017-12-27 16:59:41 +0530

    backend service pushed

--- routes/routes2_prescription_mst.js
+++ routes/routes2_prescription_mst.js
@@ -1,8 +1,9 @@
 var express=require('express');
 var router=express.Router();
 var prescription=require('../models/model_prescription_mst');
-router.get('/:id/:did',function(req,res,next){
-   
+router.get('/:id/:did?',function(req,res,next){
+   if(req.params.did)
+   {
     prescription. get_prescription_by_uid_and_did(req.params.id,req.params.did,function(err,rows){
 
             if(err)
@@ -15,5 +16,19 @@ router.get('/:id/:did',function(req,res,next){
             }
            
 });
+}
+else
+{
+    prescription.getpresc_byid(req.params.id,function(err,rows){
+        if(err){
+            res.json(err);
+        }
+        else{
+            res.json(rows);
+        }
+
+
+    });
+}
 });
 module.exports=router;
