c247dad9c8adb4de2eebdf26c1f4a0192c31c40f
malavshah9 <mdshah9574@gmail.com>
2018-01-05 09:49:51 +0530

    sads

--- models/model_medicines.js
+++ models/model_medicines.js
@@ -11,6 +11,18 @@ var medicines={
     get_medicinename:function(mid,callback){
     
         return db.query("select m.med_name,t.med_type from medicine_mst m,medicine_type t where m.pk_med_id=? and m.med_type=t.pk_med_id",[mid],callback);
+    },
+    get_medicinenames:function(item,callback)
+    {
+        /*var marr=[];
+        for(i=0;i<item.length;i++)
+        {
+               marr[i]=item[i];
+        }*/
+        var str=item;
+        var arr=[];
+        arr=str.split(',');
+ return db.query("select med_name from medicine_mst where pk_med_id IN (?)",[arr],callback);
     }
 };
 module.exports=medicines;
